c75680217907321edeb6668d5044f5f45ae27f28
JuSeong1130 <jsmoon1130@naver.com>
2022-08-11 20:07:11 +0900

    jusung merge

--- src/main/java/com/study/member/service/MemberService.java
+++ src/main/java/com/study/member/service/MemberService.java
@@ -1,7 +1,44 @@
 package com.study.member.service;
 
+import com.study.member.entity.Member;
+import com.study.member.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class MemberService {
+
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Transactional
+    public Member createMember(Member member){
+       memberRepository.findById(member.getMemberId()).orElseThrow(
+               ()-> new RuntimeException("이미 아이디가 있습니다.")
+       );
+       member =memberRepository.save(member);
+       return member;
+    }
+    @Transactional
+    public Member findMember(String memberId){
+        Member member=memberRepository.findById(memberId).orElseThrow(
+                ()-> new RuntimeException("이미 아이디가 있습니다.")
+        );
+        return member;
+    }
+    @Transactional
+    public Page<Member> findMembers(int page, int size){
+        return memberRepository.findAll(PageRequest.of(page, size, Sort.by("memberId").descending()));
+    }
+
+
+
+
 }
